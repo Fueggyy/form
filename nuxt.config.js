739f9e1c2780237fc6c38847e17cbb7c9efa6a7b
Rafif Huda <luckqmaks@gmail.com>
2022-12-20 12:17:40 +0700

    change prefix

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,8 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
-    prefix: process.env.API_URL,
+    // prefix: process.env.API_URL,
+    prefix: 'https://nuxtapi.luckqmaks.com',
     proxy: true,
   },
 
